309aa4cae4dbd89614f50d8efeda0c5b407acfd2
Kvnq Poza <Kvnqpoza@gmail.com>
2023-06-14 18:12:09 +0100

    Progress bar working correctly.

--- Assets/JS/counter.js
+++ Assets/JS/counter.js
@@ -1,12 +1,16 @@
+// Counter
+
 const counterUp = window.counterUp.default;
 
 const callback = (entries) => {
   entries.forEach((entry) => {
     const el = entry.target;
     if (entry.isIntersecting && !el.classList.contains("is-visible")) {
+      const value = el.dataset.value || 0; // Get the value from the data-value attribute or default to 0
       counterUp(el, {
-        duration: 500,
-        delay: 5,
+        duration: 1600,
+        delay: 10,
+        endVal: value, // Set the end value for the counter animation
       });
       el.classList.add("is-visible");
     }
@@ -15,5 +19,32 @@ const callback = (entries) => {
 
 const IO = new IntersectionObserver(callback, { threshold: 1 });
 
-const el = document.querySelector(".counter");
-IO.observe(el);
+const elements = document.querySelectorAll(".counter");
+elements.forEach((el) => {
+  IO.observe(el);
+});
+
+// Progress bar
+const progressBars = document.querySelectorAll(".progress-bar");
+
+progressBars.forEach((progressBar) => {
+  const value = progressBar.dataset.value || 0;
+  const htmlBar = progressBar.querySelector(".bar");
+
+  const animateProgressBar = () => {
+    htmlBar.style.width = value + "%";
+  };
+
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        animateProgressBar();
+        observer.unobserve(progressBar);
+      }
+    });
+  };
+
+  const IO = new IntersectionObserver(callback, { threshold: 1 });
+
+  IO.observe(progressBar);
+});
